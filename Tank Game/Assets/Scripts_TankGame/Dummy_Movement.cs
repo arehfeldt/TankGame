b25152a9b62c99bf94036e66f20e258b5be79a81
Grant Gabel <grantg1016@gmail.com>
2020-11-27 19:19:39 -0500

    Enemy now targets player
    Enemy tank now targets provided player character, just need to make sure on build to set tag of player to "player", currently default is "untagged"

---
+++ Tank Game/Assets/Scripts_TankGame/Dummy_Movement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dummy_Movement : MonoBehaviour
+{
+    public GameObject dummy;
+    public float moveSpeed = 5f;
+    Vector3 dummyPosition;
+    Vector3 dummyVelocity;
+    int direction;  // 0 = forward, 1 = right, 2 = backward, 3 = left
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dummy.transform.position = new Vector3(-20, 0, -20);
+        dummyPosition = dummy.transform.position;
+        dummyVelocity = Vector3.zero;
+        direction = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // position / movement update
+        switch (direction)
+        {
+            case 0:
+                dummy.transform.Translate(0f, 0f, moveSpeed * Time.deltaTime);
+                break;
+            case 1:
+                dummy.transform.Translate(moveSpeed * Time.deltaTime, 0f, 0f);
+                break;
+            case 2:
+                dummy.transform.Translate(0f, 0f, -moveSpeed * Time.deltaTime);
+                break;
+            case 3:
+                dummy.transform.Translate(-moveSpeed * Time.deltaTime, 0f, 0f);
+                break;
+            default:
+                break;
+        }
+
+        dummyPosition = dummy.transform.position;
+
+        // direction update
+        switch (direction)
+        {
+            case 0:
+                if (dummyPosition.z > 20f)
+                {
+                    direction = 1;
+				}
+                break;
+            case 1:
+                if (dummyPosition.x > 20f)
+                {
+                    direction = 2;
+                }
+                break;
+            case 2:
+                if (dummyPosition.z < -20f)
+                {
+                    direction = 3;
+                }
+                break;
+            case 3:
+                if (dummyPosition.x < -20f)
+                {
+                    direction = 0;
+                }
+                break;
+            default:
+                break;
+		}
+
+    }
+}
---
+++ Tank Game/Assets/Scripts_TankGame/Enemy_Tank_Shooting.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Tank_Shooting : MonoBehaviour
+{
+    // assumes original rotation aka rotation of [0, 0, 0] has turret poiting towards [0, 0, 1]
+    public GameObject enemy_tank_turret;
+    public float aggroRadius = 45f;
+    Transform playerTransform;
+    bool aggro = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectsWithTag("Player")[0].transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (aggro)
+        {
+            enemy_tank_turret.transform.LookAt(playerTransform, Vector3.up);
+        }
+        
+        if (Vector3.Distance(playerTransform.position, enemy_tank_turret.transform.position) < aggroRadius)
+        {
+            aggro = true;
+		}
+        else
+        {
+            aggro = false;
+		}
+    }
+
+}
