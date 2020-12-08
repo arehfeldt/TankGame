d5a8ac1cddfb410ad6c46cb275ff57ca80daf5a1
Grant Gabel <grantg1016@gmail.com>
2020-12-08 15:54:04 -0500

    added materials and started work on shooting

---
+++ Tank Game/Assets/Tank!_Assets/Scirpts/ShellExplosion.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ShellExplosion : MonoBehaviour
+{
+    public LayerMask m_TankMask;
+    public ParticleSystem m_ExplosionParticles;       
+    public AudioSource m_ExplosionAudio;              
+    public float m_MaxDamage = 100f;                  
+    public float m_ExplosionForce = 1000f;            
+    public float m_MaxLifeTime = 2f;                  
+    public float m_ExplosionRadius = 5f;              
+
+
+    private void Start()
+    {
+        Destroy(gameObject, m_MaxLifeTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Find all the tanks in an area around the shell and damage them.
+        // change tank mask, currenlty used in tutorial to find all tanks, so need it to be only player 
+        Collider[] colliders = Physics.OverlapSphere(transform.position, m_ExplosionRadius, m_TankMask);
+    }
+
+
+    private float CalculateDamage(Vector3 targetPosition)
+    {
+        // Calculate the amount of damage a target should take based on it's position.
+        return 0f;
+    }
+}
